2160a69135499351ef846a7d8f81dd5491faea3d
m.choromzadeh <chorom.mamad@gmail.com>
2019-07-07 19:24:58 +0430

    create posts component

---
+++ src/components/posts.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import getPosts from '../services/fakePosts'
+
+class Posts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            posts:[],
+            currentPage:1,
+            pageSize:5
+         }
+    }
+
+    componentDidMount(){
+        const posts = getPosts();
+        this.setState({ posts });
+    }
+    render() { 
+        const {posts} = this.state; 
+        return (
+            
+            <React.Fragment>
+                {posts.map(post =>(
+            <div class="container-fluid">
+                <div className="card shadow-lg bg-light m-2">
+                <article className="p-3">
+                        <div className="card-header">
+                            <h3 className="card-title">
+                                
+                                <a href="#">{post.postTitle}</a>
+                            </h3>
+                           <span className="card-subtitle">
+                                <span className="fa fa-calendar m-2" />
+                                {post.postDate}
+
+                           </span>
+                           <img
+                                className="card-img"
+                                src={post.postImageUrl}
+                                alt=""
+                            />
+                        </div>
+                        <div className="card-body">
+                            <p className="card-text">
+                                {post.postContent}
+                            </p>
+                        </div>
+                        <div className="card-footer">
+                            <ul className="list-inline float-right">
+                                <li className="list-inline-item">
+                                    <span className="fa fa-link m-1" />
+                                    برچسب ها:
+                                </li>
+                                <li className="list-inline-item">
+                                    <a href="#">{post.postTags}</a>
+                                </li>
+                                
+                            </ul>
+                        </div>
+                </article>
+                </div>
+            </div>
+            ))}
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default Posts;
