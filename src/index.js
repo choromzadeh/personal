938148c330b50d2ad1a8992b3d99118506012701
m.choromzadeh <chorom.mamad@gmail.com>
2019-07-08 13:20:51 +0430

    refactor all contents

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap-v4-rtl/dist/css/bootstrap-rtl.css';
+import 'font-awesome/css/font-awesome.css';
+import './css/index.css';
+import './css/navbar.css';
+import './css/sidebar.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
