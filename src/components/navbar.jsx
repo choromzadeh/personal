b5ca0c904c7554aca785df0e9d00180aa5b759ab
choromzadeh <chorom.mamad@gmail.com>
2019-07-09 13:01:36 +0430

    link login to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
             <SearchBar></SearchBar>
             <ul className="navbar-nav px-3">
                 <li className="nav-item text-nowrap">
-                {/* <Link className="nav-link" to="/login">
+                <Link className="nav-link" to="/login">
                         ورود
-                    </Link> */}
-                <a className="nav-item text-nowrap">خروج</a>
+                    </Link>
+               
                 </li>
             </ul>
             
